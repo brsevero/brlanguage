b5634d09d342558d320cb897fa186f51047debcd
brsevero <brsss@ic.ufal.br>
2021-03-28 01:25:36 -0300

    começando o analizador

--- main.py
+++ main.py
@@ -1,57 +1,6 @@
-"""""
-1. Indetificadores não podem conter numeros 
-2. A linguagem é toda em português
-3. Tipos de dados primitivos:
-  1. inteiro
-  2. ponto flutuante
-  3. caractere
-  4. booleano
-  5. cadeia de caractere
-  6. Arranjos unidimencionais
+import BasicLexer as bl
+lexer = bl.BasicLexer()
 
-4. COnjunto de operadores: #colocar a ordem de precedência e associatividade
-  1. Aritméticos:
-    1. "*" multiplicação
-    2. "/" divisão
-    3. "-" subtração
-    4. "+" adição
-    5. "%" resto da divisão
-
-  2. Relacionais:
-    1. inteiros:
-      1. "=="
-      2. "!="
-      3. "<"
-      4. ">"
-      5. "<="
-      6. ">="
-    2. Caractere:
-      1. Não Possui
-    3. Cadeia de caracteres:
-      1. Não possui
-
-  3. Lógicos:
-    1. "!" negação
-    2. "&&" and
-    3. "||" or
-
-  4. Concatenação:
-    1. Caractere e Cadeia de caracteres:
-      1. "+"
-    2. Numéricos e Booleanos:
-      1. Não possui
-
-
-. Contantes Literais de cada Tipo:
-
-. Operacoes de cada tipo
-
-funcao bruno(argumentos):
-  imprime("Olá mundo!\n");
-
-
-
-
-https://www.geeksforgeeks.org/how-to-create-a-programming-language-using-python/
-https://pt.wikipedia.org/wiki/Express%C3%A3o_regular
-"""""
+text = "1 + 2"
+print(lexer.tokenize(text))
+lexer.tokenize(text)
--- teste.py
+++ teste.py
@@ -1,26 +1,44 @@
 from sly import Lexer
 
 class Meulexico(Lexer):
-    #lista de tokens que eu preciso obrigatoriamente
+    #lista de tokens que eu preciso obrigatoriamente e sempre em UPCASE
     #lista de coisas ignoradas em ER sempre com aspas simples
     #lista de ER para os tokens
 
-    tokens = {ID, ATRIBUICAO, NUMERO, SOMA}
+    tokens = {ID, ATRIBUICAO, NUMERO, SOMA, IF, ELSE, WHILE}
 
+    #ignorar coisa só precisa comecar com "ignore_"
     ignore = ' \t'
+    ignore_comment = r'\#.*'
+    ignore_newline = r'\n+'
 
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
     ATRIBUICAO = r'='
     NUMERO = r'\d+'
     SOMA = r'\+'
 
+    # Special cases
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+
+    def NUMERO(self, t):
+        t.value = int(t.value)
+        return t
+
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)    
+
 
 if __name__ == '__main__':
-    texto = "x = 1 + 2"
+    texto = """x = 1 + 2
+            #comentario"""
     compilador = Meulexico()
     #lista_de_tokens = []
     for x in compilador.tokenize(texto):
         #lista_de_tokens.append((x.type,x.value))
         print('tipo = %r, valor = %r' % (x.type,x.value))
     #print(lista_de_tokens)
+    print(compilador.lineno)
+    print(compilador.)
 
