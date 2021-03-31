296bef45fc6601aa0a5b3c1616e9937230828e85
Lael Santa Rosa <laelrosa@communicatti.com>
2021-03-30 21:47:45 -0300

    char and string

--- lexer.py
+++ lexer.py
@@ -2,7 +2,7 @@ from sly import Lexer
 
 class analisador_lexico(Lexer):
 
-    tokens = {VARIAVEL, FUNCAO, ATRIBUICAO, IGUALADOR, INTEIRO, MAIS, MENOS, VEZES,DIVIDIR, PARENTESES_ESQ, PARENTESES_DIR}
+    tokens = {VARIAVEL, FUNCAO, ATRIBUICAO, IGUALADOR, INTEIRO, FLUTUANTE, CARACTERE, BOOLEANO, CARACTERES, MAIS, MENOS, VEZES,DIVIDIR, PARENTESES_ESQ, PARENTESES_DIR}
     # SE, MAS_SE, SENAO, ENQUANTO, IDENTADOR, IMPRIMIR
     literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';'}
 
@@ -14,6 +14,9 @@ class analisador_lexico(Lexer):
     ATRIBUICAO = r'='
     IGUALADOR = r'=='
     INTEIRO = r'\d+'
+    # FLUTUANTE = r'\d+'
+    CARACTERE = r'[\'|\"]\S[\'|\"]'
+    CARACTERES = r'[\'\"].*?[\'\"]'
     MAIS = r'\+'
     MENOS = r'-'
     VEZES = r'\*'
@@ -22,13 +25,16 @@ class analisador_lexico(Lexer):
     PARENTESES_DIR = r'\)'
 
 
+
+    VARIAVEL['FALSO'] = BOOLEANO
+    VARIAVEL['VERDADEIRO'] = BOOLEANO
     VARIAVEL['SE'] = FUNCAO
     VARIAVEL['MAS_SE'] = FUNCAO
     VARIAVEL['SENAO'] = FUNCAO
     VARIAVEL['ENQUANTO'] = FUNCAO
     VARIAVEL['IDENTADOR'] = FUNCAO
     VARIAVEL['IMPRIMIR'] = FUNCAO
-
+    VARIAVEL['PRINCIPAL'] = FUNCAO
 
     def NUMERO(self, t):
         t.value = int(t.value)
@@ -39,7 +45,8 @@ class analisador_lexico(Lexer):
 
 
 if __name__ == '__main__':
-    data = """IMPRIMIR"""
+    data = """('alo mundo')
+"""
     lexer = analisador_lexico()
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
