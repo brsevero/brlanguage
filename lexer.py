5594e4d35f4912f922f30db43263265416384cd8
brsevero <brsss@ic.ufal.br>
2021-04-02 16:09:14 -0300

    finalizando

--- lexer.py
+++ lexer.py
@@ -1,88 +1,150 @@
-from sly import Lexer
+import re
 
-class analisador_lexico(Lexer):
+class Token():
+    def __init__(self):
+        self.tokens = []
+        self.linhas = int(0)
+        self.colunas = int(0)
 
-    tokens = {VARIAVEL, FUNCAO, RESERVADA, ATRIBUICAO, IGUALADOR, VAZIO, INTEIRO, FLUTUANTE, CARACTERE, BOOLEANO, CARACTERES, MAIS, MENOS, VEZES,DIVIDIR, PARENTESES_ESQ, PARENTESES_DIR}
-    # SE, MAS_SE, SENAO, ENQUANTO, IDENTADOR, IMPRIMIR
-    literals = { '=', '+', '-', '*', '/', '(', ')','{','}','>','<','==','!=','>=','<=', ',', ';',"'","\"",}
+    def regras(self, token):
+        NAO_RECONHECIDO = (0, 'nao reconhecido')
+        VARIAVEL = (1, 'variavel')
+        IGUALADOR = (2, 'igualador')
+        MAIOR_QUE = (3, 'maior que')
+        MENOR_QUE = (4, 'menor que')
+        DIFERENTE = (5, 'diferente')
+        MAIOR_IGUAL = (6, 'maior igual')
+        MENOR_IGUAL = (7, 'menor igual')
+        ATRIBUICAO = (8, 'atribuicao')
+        NUMERO = (9, 'tipo: numero')
+        CARACTERE = (10, 'tipo: caractere')
+        CARACTERES = (11, 'tipo: caracteres')
+        MAIS = (12,'mais')
+        MENOS = (13, 'menos')
+        VEZES = (14, 'vezes')
+        DIVIDIR = (15, 'dividir')
+        PARENTESES_ESQ = (16, 'parenteses esquerdo')
+        PARENTESES_DIR = (17, 'parenteses direito')
+        CHAVE_ESQ = (18, 'chave esquerda')
+        CHAVE_DIR = (19, 'chave direita')
+        CONCHETE_DIR = (20, 'conchete direito')
+        CONCHETE_ESQ = (21, 'conchete esquerdo')
+        VIRGULA = (22, 'virgula')
+        PONTO_VIR = (23, 'ponto e virgula')
+        ASPAS = (24, 'aspas')
+        ASPAS_DU = (25, 'aspas duplas')
+        CONCATENACAO = (26, 'concatenacao')
+        FUNCAO = (27, 'funcao')
+        PRINCIPAL = (28, 'principal')
+        LER = (29, 'leitura')
+        IMPRIMIR = (30, 'escrita')
+        RETORNE = (31, 'retorno')
+        SE = (32, 'condicional se')
+        MAS_SE = (33, 'condicional mas se')
+        SENAO = (34, 'condicional senao')
+        FAZER = (35, 'repeticao faz')
+        ENQUANTO = (36, 'repeticao enquanto')
+        ITERADOR = (37, 'repeticao iterador')
+        BOOLEANO = (38, 'tipo: booleano')
+        NAO = (39, 'logico nao')
+        E = (40, 'logico e')
+        OU = (41, 'logico ou')
+        VAZIO = (42, 'tipo: vazio')
+        INTEIRO = (43, 'tipo: inteiro')
+        BRANCO = (44, 'branco')
+        COMENTARIO = 0
 
-    ignore = ' \t'
-    ignore_comment = r'\#.*'
 
-    @_(r'\n+')
-    def ignore_newline(self, t):   
-        self.lineno += len(t.value)
+        palavras_reservadas = {
+            'FUNCAO' : FUNCAO,
+            'PRINCIPAL' : PRINCIPAL,
+            'LER' : LER,
+            'IMPRIMIR' : IMPRIMIR,
+            'RETORNE' : RETORNE,
+            'SE' : SE,
+            'MAS_SE' : MAS_SE,
+            'SENAO' : SENAO,
+            'FAZER' : FAZER,
+            'ENQUANTO' : ENQUANTO,
+            'ITERADOR' : ITERADOR,
+            'VERDADEIRO' : BOOLEANO,
+            'FALSO' : BOOLEANO,
+            'NAO' : NAO,
+            'E' : E,
+            'OU' : OU,
+            'VAZIO' : VAZIO,
+            'INTEIRO' : INTEIRO,
+        }
 
-    @_(r'FUNCAO [a-zA-Z_][a-zA-Z0-9_]*')
-    def FUNCAO(self, t):
-        t.value = t.value[7:]
-        return t
+        regras = {
+            '[a-zA-Z][a-zA-Z0-9_]*' : VARIAVEL,
+            '\==' : IGUALADOR,
+            '\>' : MAIOR_QUE,
+            '\<' : MENOR_QUE,
+            '\!=' : DIFERENTE,
+            '\>=' : MAIOR_IGUAL,
+            '\<=' : MENOR_IGUAL,
+            '\=' : ATRIBUICAO,
+            '\d+' : NUMERO,
+            '[\'|\"]\S[\'|\"]' : CARACTERE,
+            '[\'\"].*?[\'\"]' : CARACTERES,
+            '\+' : MAIS,
+            '\-' : MENOS,
+            '\*' : VEZES,
+            '\/' : DIVIDIR,
+            '\(' : PARENTESES_ESQ,
+            '\)' : PARENTESES_DIR,
+            '\{' : CHAVE_ESQ,
+            '\}' : CHAVE_DIR,
+            '\[' : CONCHETE_DIR,
+            '\]' : CONCHETE_ESQ,
+            '\,' : VIRGULA,
+            '\;' : PONTO_VIR,
+            '\'' : ASPAS,
+            '\"' : ASPAS_DU,
+            '\&' : CONCATENACAO,
+            '\#' : COMENTARIO
+            }
 
-    VARIAVEL = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    ATRIBUICAO = r'='
-    IGUALADOR = r'=='
-    # FLUTUANTE = r'\d+'
-    CARACTERE = r'[\'|\"]\S[\'|\"]'
-    CARACTERES = r'[\'\"].*?[\'\"]'
-    MAIS = r'\+'
-    MENOS = r'-'
-    VEZES = r'\*'
-    DIVIDIR = r'/'
-    PARENTESES_ESQ = r'\('
-    PARENTESES_DIR = r'\)'
+        for i in palavras_reservadas:
+            if i == token:
+                return palavras_reservadas[i]
+
+        for i in regras:
+            if re.match(i, token):
+                return regras[i]
+        
+        return NAO_RECONHECIDO
     
+    def separar(self,linha, delimitador):
+        linha = linha.replace(delimitador," " + delimitador + " ")
+        return linha
 
-    
 
-    VARIAVEL['FALSO'] = BOOLEANO
-    VARIAVEL['VERDADEIRO'] = BOOLEANO
-    VARIAVEL['SE'] = RESERVADA
-    VARIAVEL['MAS_SE'] = RESERVADA
-    VARIAVEL['SENAO'] = RESERVADA
-    VARIAVEL['ENQUANTO'] = RESERVADA
-    VARIAVEL['IDENTADOR'] = RESERVADA
-    VARIAVEL['IMPRIMIR'] = RESERVADA
-    VARIAVEL['PRINCIPAL'] = RESERVADA
-    VARIAVEL['FUNCAO'] = RESERVADA
-    VARIAVEL['VAZIO'] = RESERVADA
-    VARIAVEL['RETORNE'] = RESERVADA
+    def criar_tokens(self, line):
+        delimitadores = ['(',')','{','}','[',']',',',';','+','-','*','/']
+        for i in delimitadores:
+            line = self.separar(line,i)
+        return line.split() 
 
-    @_(r'\d+')
-    def INTEIRO(self, t):
-        t.value = int(t.value)
-        return t
+    def nextToken(self, linha):
+        #criar um if para quando reconhecer o comentario pular a linha
+        print(linha)
+        self.linhas += 1
+        self.tokens.extend(self.criar_tokens(linha))
+        for i in self.tokens:
+            print('[{}, coluna: {}'.format(self.linhas,linha.find(i)), end=' ')
+            print(self.regras(i),i,end='')
+            input()
+        self.tokens = []
 
-    def ignore_newline(self, t):
-        self.lineno += len(t.value)    
+
 
 
 if __name__ == '__main__':
-    data = """VAZIO FUNCAO fibonacci(INTEIRO limite){
-   INTEIRO contador;
-   INTEIRO um;
-   INTEIRO dois;
-   INTEIRO tres;
-
-   um = 1;
-   dois = 1;
-
-   if(limite == 0){
-      IMPRIMIR("0");
-   }
-   ENQUANTO(contador < limite){
-      SE(contador < 2){
-         IMPRIMIR("1");
-      }
-      SENAO{
-         tres = um + dois;
-         um = dois;
-         dois = tres;
-         IMPRIMIR(tres +++ " ");
-      }
-   }
-   contador = contador + 1;
-}
-"""
-    lexer = analisador_lexico()
-    for tok in lexer.tokenize(data):
-        print('type=%r, value=%r' % (tok.type, tok.value))
+    a = Token()
+    with open('shell.brl', 'r') as file:
+        for linha in file:
+            a.nextToken(linha)
+            
+        
--- main.py
+++
@@ -1,8 +0,0 @@
-from lexico import analisador_lexico
-
-if __name__ == '__main__':
-    with open('shell.brl','r') as data:
-        lexer = analisador_lexico()
-        for linhas in data:
-            for tok in lexer.tokenize(linhas):
-                print('type=%r, value=%r, linha = %i' % (tok.type, tok.value, lexer.linha))
